6f58862b6993483135b01c6f98f68dd4c1015efa
DavidDock <ddocksey@live.co.uk>
2024-03-18 09:22:57 +0000

    Add posts list view and url

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from cheers_api.permissions import IsOwnerOrReadOnly
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+
+class PostList(generics.ListCreateAPIView):
+    """
+    List posts or create a post if logged in
+    The perform_create method associates the post with the logged in user.
+    """
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Post.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
